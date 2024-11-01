0c51d7608de3112bd14a987e26ce0bf90d6c43ba
中島浩二 <knaka0209@gmail.com>
2024-11-01 18:08:18 +0900

    up, 1101a15

---
+++ plan4/src/App.tsx
@@ -0,0 +1,49 @@
+import { Routes, Route } from 'react-router-dom';
+
+//
+import Home from './client/home';
+import BookMark from './client/BookMark';
+import Chat from './client/Chat';
+import Cms from './client/Cms';
+import FormTest1 from './client/FormTest1';
+import FormTest3 from './client/FormTest3';
+import FormTest4 from './client/FormTest4';
+import FormTest5 from './client/FormTest5';
+import FormTest6 from './client/FormTest6';
+import FormTest6crate from './client/FormTest6/Create';
+import FormTest6edit from './client/FormTest6/Edit';
+
+import Plan from './client/Plan';
+import Plan4 from './client/Plan4';
+import Todo from './client/Todo';
+//
+import Test1 from './client/Test1';
+//
+export default function App(){
+    return(
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/cms" element={<Cms />} />
+        <Route path="/bookmark" element={<BookMark />} />
+        <Route path="/form_test1" element={<FormTest1 />} />
+        <Route path="/form_test3" element={<FormTest3 />} />
+        <Route path="/form_test4" element={<FormTest4 />} />
+        <Route path="/form_test5" element={<FormTest5 />} />
+        <Route path="/form_test6" element={<FormTest6 />} />
+        <Route path="/form_test6create" element={<FormTest6crate />} />
+        <Route path="/form_test6edit" element={<FormTest6edit />} />
+        <Route path="/plan" element={<Plan />} />
+        <Route path="/plan4" element={<Plan4 />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/test1" element={<Test1 />} />
+      </Routes>
+    </div>
+    )
+}
+/*
+<Route path="/test3" element={<Test3 />} />
+<Route path="/test4" element={<Test4 />} />
+<Route path="/datatable" element={<DataTable />} />
+*/
---
+++ plan4/src/components/Head.tsx
@@ -0,0 +1,23 @@
+//import { Routes, Route, Link } from 'react-router-dom';
+import {Link } from 'react-router-dom';
+//
+function Page() {
+    return (
+    <div>
+        <a href="/">[ Home ]</a>
+        <a href="/chat" className="ms-2"> [ chat ]</a>
+        <a href="/todo" className="ms-2"> [ todo ]</a>
+        <a href="/bookmark" className="ms-2"> [ bookmark ]</a>
+        <a href="/cms" className="ms-2"> [ cms ]</a>
+        <a href="/form_test1" className="ms-2"> [ Form1 ]</a>
+        <a href="/form_test3" className="ms-2"> [ Form3 ]</a>
+        <a href="/form_test4" className="ms-2"> [ Form4 ]</a>
+        <a href="/form_test5" className="ms-2"> [ Form5 ]</a>
+        <a href="/form_test6" className="ms-2"> [ Form6 ]</a>
+        <a href="/plan" className="ms-2"> [ plan ]</a>
+        <a href="/plan4" className="ms-2"> [ plan4 ]</a>
+        <br />
+    </div>
+    );
+}
+export default Page;
